6ff1213d999665c3917e47adcc492e8d51af53f1
SeanCarlson96 <92127265+SeanCarlson96@users.noreply.github.com>
2022-06-27 16:25:39 -0500

    s

--- js/slideshow.js
+++ js/slideshow.js
@@ -15,7 +15,9 @@ function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
+  
   if (n > slides.length) {slideIndex = 1}
+  console.log(slides.length)
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
